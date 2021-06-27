e4c4bbae62ec043176c1b9f278e7cc3ea4e9c992
leilavajed <77764419+leilavajed@users.noreply.github.com>
2021-06-26 20:48:27 -0700

    first commit

---
+++ data/process_data.py
@@ -0,0 +1,60 @@
+import sys
+import pandas as pd
+import sqlalchemy
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return pd.merge(messages,categories,on='id')
+
+def is_notone(inputs):
+    if inputs == '2':
+        return '1'
+    else:
+        return inputs
+def clean_data(df):
+    categories = df['categories'].str.split(";",expand=True,)
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x:x.split('-')[0])
+    categories.columns = category_colnames
+    for column in categories:
+        categories[column] = categories[column].apply(lambda x:is_notone(x.split('-')[1]))
+        categories[column] = categories[column].astype(int)
+    df = df.drop('categories', axis=1)
+    df =  pd.concat([df,categories],sort=False ,axis=1)
+    return df.drop_duplicates()
+
+
+def save_data(df, database_filename):
+    engine = sqlalchemy.create_engine('sqlite:///'+database_filename)
+    df.to_sql('Insert_to_Table', engine, index=False, if_exists='replace')
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,108 @@
+import sys
+import pickle
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+nltk.download(['punkt', 'wordnet'])
+import re
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+
+
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///'+ database_filepath)
+    df = pd.read_sql_table(table_name=engine.table_names()[0],con='sqlite:///'+database_filepath)
+    X = df['message'].values
+    Y = df[(df.columns[4:])].values
+    return X,Y,df.columns[4:]
+
+
+def tokenize(text):
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = nltk.word_tokenize(text)
+    lemmatizer = nltk.WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    max_features = ['auto', 'sqrt']
+    max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+    max_depth.append(None)
+    min_samples_split = [2, 5, 10]
+    min_samples_leaf = [1, 2, 4]
+    bootstrap = [True, False]
+    parameters = { 'clf__estimator__max_features': max_features,
+                   'clf__estimator__max_depth': max_depth,
+                   'clf__estimator__min_samples_split': min_samples_split,
+                   'clf__estimator__min_samples_leaf': min_samples_leaf,
+                   'clf__estimator__bootstrap': bootstrap}
+
+
+    cv = GridSearchCV(pipeline, param_grid=parameters ,cv = 3)
+    return cv
+    
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    for i in range(len(Y_test)):
+        print(classification_report(y_pred=y_pred[:,i],y_true=y_test[:,i]))
+    
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
